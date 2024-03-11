f8cfb520316cc3f35fc8c873a995ab3adfeb7017
huy.start17 <huyhuy17@gmail.com>
2024-03-11 20:09:56 +0700

    .

--- invincible-unit-example/src/invincibleunitexample/content/IUEUnitTypes.java
+++ invincible-unit-example/src/invincibleunitexample/content/IUEUnitTypes.java
@@ -1,18 +1,13 @@
 package invincibleunitexample.content;
 
-import mindustry.ai.*;
-import mindustry.content.*;
-import mindustry.gen.*;
-import mindustry.graphics.*;
 import mindustry.type.*;
-import mindustry.world.meta.*;
 
 import invincibleunitexample.unit.*;
 
 public class IUEUnitTypes {
     public static UnitType invincibleAircraft;
 
-    public static load() {
+    public static void load() {
         invincibleAircraft = new InvincibleAircraft("invincible-aircraft");
     }
 }
